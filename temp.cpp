b501d925b04708e971adf7528be5adff22b03172
HaiderYar <128994427+HaiderYar@users.noreply.github.com>
2024-12-12 14:47:48 +0500

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,36 +1,15 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+using namespace std;
 
-// Triangle class
-class Triangle {
-private:
-    double base;
-    double height;
 
-public:
-    // Constructor
-    Triangle(double b, double h) : base(b), height(h) {}
-
-    // Method to calculate area
-    double calculateArea() {
-        return 0.5 * base * height;
+int evenOdd(int num)
+{
+    if(num%2 == 0)
+    {
+        return 1;
     }
-};
-
-// Palindrome class
-class Palindrome {
-public:
-    // Method to check if a string is palindrome
-    static bool isPalindrome(const std::string& str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str[left] != str[right])
-                return false;
-            left++;
-            right--;
-        }
-        return true;
+    else
+    {
+        return 0;
     }
-};
+}
